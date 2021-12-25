9404052fa06575872b67702f5f565fa32ed6ab06
Isaac Yee <48425750+IY314@users.noreply.github.com>
2021-12-25 10:17:57 -0800

    Add login capability (with some bugs)

--- src/mythos.py
+++ src/mythos.py
@@ -4,6 +4,8 @@ import importlib
 import time
 import json
 from blessed import Terminal
+import os
+import hashlib
 
 
 PATHS = [
@@ -133,6 +135,123 @@ def callterm(*calls, newline=False):
         print()
 
 
+def clearterm(term):
+    callterm(term.home, term.clear, term.normal)
+    callterm(term.center(' MythOS v1.0.0 ', fillchar=f'='), newline=True)
+    print()
+
+
+def init(filename='mythos/account_data.json'):
+    with open(filename, 'w+') as f:
+        json.dump({'current': None, 'all': []}, f, indent=4)
+
+
+def select_account(data, term, account_idx):
+    if account_idx is None:
+        account_idx = 0 if data.get('all') else -1
+    while True:
+        clearterm(term)
+        callterm(term.center('Select Account'), newline=True)
+        for i, a in enumerate(data['all']):
+            if account_idx == i:
+                callterm(term.black_on_white, a['username'], term.normal, newline=True)
+            else:
+                print(a['username'])
+        if account_idx == -1:
+            callterm(term.black_on_white, 'Create new account...', term.normal, newline=True)
+        else:
+            print('Create new account...')
+        key = term.inkey()
+        if key.code == term.KEY_DOWN and -1 < account_idx < len(data['all']):
+            account_idx += 1
+            if account_idx == len(data['all']):
+                account_idx = -1
+        elif key.code == term.KEY_UP and (account_idx > 0 or account_idx == -1):
+            account_idx -= 1
+            if account_idx < 0:
+                account_idx = len(data['all']) - 1
+        elif key.code == term.KEY_ENTER:
+            return account_idx
+
+
+def login(account_idx, data, term, filename):
+    clearterm(term)
+    password = data['all'][account_idx]['password']
+    user_input = ''
+    print(f'Hi, {data["all"][account_idx]["username"]}')
+    print('Enter your password')
+
+    while (key := term.inkey()).code != term.KEY_ENTER:
+        if not key.is_sequence:
+            user_input += key
+        elif key.code == term.KEY_ESCAPE:
+            return -1
+    
+    password_attempt = hashlib.sha256(user_input.encode('utf-8')).hexdigest()
+    if password_attempt == password:
+        data['current'] = data['all'][account_idx]
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+        return 0
+    return 1
+
+
+def get_username(term):
+    username = ''
+    callterm('Enter a username: ')
+    while (key := term.inkey()).code != term.KEY_ENTER:
+        if not key.is_sequence:
+            username += key
+            callterm(key)
+        elif key.code == term.KEY_BACKSPACE:
+            username = username[:-1]
+            callterm('\x08')
+        elif key.code == term.KEY_ESCAPE:
+            return -1
+    
+    return username
+
+
+def get_password(term):
+    password = ''
+
+    callterm('Enter a password: ')
+    while (key := term.inkey()).code != term.KEY_ENTER:
+        if not key.is_sequence:
+            password += key
+            callterm('*')
+        elif key.code == term.KEY_BACKSPACE:
+            password = password[:-1]
+            callterm('\x08')
+        elif key.code == term.KEY_ESCAPE:
+            return -1
+
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+
+def create_new_account(data, term, filename):
+    while True:
+        clearterm(term)
+
+        username = get_username(term)
+        if username == -1:
+            return -1
+
+        password = get_password(term)
+        if password == -1:
+            continue
+        
+        user = {'username': username, 'password': password}
+        data['all'].append(user)
+        data['current'] = user
+
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+        
+        return 0
+        
+
+
 def boot(filename='mythos/account_data.json'):
     data = None
 
@@ -141,13 +260,24 @@ def boot(filename='mythos/account_data.json'):
 
     term = Terminal()
     with term.cbreak(), term.fullscreen():
+        account_idx = None
         while True:
-            callterm(term.home, term.clear)
-            callterm(term.center(' MythOS v1.0.0 ', fillchar=f'='), newline=True)
-            callterm(term.center('Select Account'), newline=True)
-            for a in data.get('all', []):
-                pass
+            account_idx = select_account(data, term, account_idx)
+            if account_idx == -1:
+                status = create_new_account(data, term, filename)
+            else:
+                status = login(account_idx, data, term, filename)
+
+            if status == -1:
+                continue
+
+            break
+
+        clearterm(term)
+
 
 
 if __name__ == '__main__':
+    if not os.path.exists('./mythos/account_data.json'):
+        init()
     boot()
